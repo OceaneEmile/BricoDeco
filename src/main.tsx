4632554f895b19df89e3323b993136171d4c4c4b
Fir4tAsl <firat.aslan@oclock.school>
2024-02-06 11:04:54 +0100

    router and page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter,RouterProvider } from 'react-router-dom'
 import App from './App.tsx'
+import ErrorPage from './Components//Error/ErrorPage.tsx'
+import Home from './Components/Main/Home.tsx'
 import './index.css'
+
+// Ha .. t'as disparu ?
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<App />,
+    errorElement:<ErrorPage />,
+    children:[
+      {
+        path:"/",
+        element:<Home />
+      },
+      {
+        path:"error",
+        element: <ErrorPage/>
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
